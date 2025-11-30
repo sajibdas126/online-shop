96dc3a802d6f40e59e0b021d136b191734f14315
Sajib <sajibdashtrg@gmail.com>
2025-11-30 22:58:35 +0600

    Refactor code structure for improved readability and maintainability

---
+++ src/Pages/Home/HomeComponent/Collection.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import women from "../../../../public/collection/woman.jpg"
+import women from "../../../../public/collection/men.jpg"
+import women from "../../../../public/collection/kids.webp"
+
+
+
+const Collection = () => {
+  return (
+    <div>
+        <div>
+      
+        </div>
+    </div>
+  )
+}
+
+export default Collection
--- src/Pages/Home/HomeComponent/NewProduct.jsx
+++ src/Pages/Home/HomeComponent/NewProduct.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
 import { SectionHeading } from './SectionHeading'
+import { useData } from '../../../Hooks/useData';
+import { ProductCart } from '../../../Components/SherComponent/ProductCart';
+
 
 const NewProduct = () => {
-    const {products} = useData()
+    const {products} = useData();
+    
 
-    }
+
+    
   return (
     <div>
         <div className='container mx-auto px-24 '>
             <div>
                 <SectionHeading discription={"Don't wait. the time will never be just right."} heading={"Day of the deal"} colorHeading={"The deal"}></SectionHeading>
             </div>
+          <div className='py-12'>
+             <div className='flex flex-wrap gap-4 justify-center'>
+          {
+            products.map(p=><ProductCart  product={p}>
+
+            </ProductCart>) 
+          }
+        </div>
+          </div>
         </div>
     </div>
-  )
+  );
 }
-
 export default NewProduct
